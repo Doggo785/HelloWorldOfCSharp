a48c1ef381278a546fbbd101b7c3656de9f44b75
Doggo <stephane.plathey@proton.me>
2025-05-19 19:56:00 +0200

    Exercice 2 - Consolas and Telim

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string name;
+﻿// Exercice 1 - Hello World
+/*
+string name;
 name = "Doggo";
 
 Console.WriteLine("Hello, World!");
@@ -7,3 +9,12 @@ Console.WriteLine("Im the one that will save your Wolrd!");
 Console.WriteLine("I Will defeat the Uncoded One, i Promise..");
 Console.WriteLine("I will be the one to save you all!");
 Console.WriteLine("My name is " + name + "!");
+*/
+
+// Exercice 2 - Consolas and Telim
+string client;
+
+Console.WriteLine("Bread is ready!");
+Console.WriteLine("Who is the Bread for ?");
+client = Console.ReadLine();
+Console.WriteLine("Noted : The bread is for " + client + "!");
