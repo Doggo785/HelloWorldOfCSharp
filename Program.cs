34ea1d9e9813b6573ad8eb5dbd5ade3905f45b0f
Doggo <stephane.plathey@proton.me>
2025-05-19 19:45:44 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string name = "Doggo";
+string Strangers
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Im a GOD!");
+Console.WriteLine("Im the one that will save your Wolrd!");
+Console.WriteLine("I Will defeat the Uncoded One, i Promise..");
+Console.WriteLine("I will be the one to save you all!");
+Console.WriteLine("My name is " + name + "!");
