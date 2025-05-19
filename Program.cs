b9a1e607b6e637fedd38c31c2a621d81c1ae52e8
Doggo <stephane.plathey@proton.me>
2025-05-19 20:09:34 +0200

    Exercice 3 - The Thing Namer 3000

--- Program.cs
+++ Program.cs
@@ -12,9 +12,21 @@ Console.WriteLine("My name is " + name + "!");
 */
 
 // Exercice 2 - Consolas and Telim
+/*
 string client;
 
 Console.WriteLine("Bread is ready!");
 Console.WriteLine("Who is the Bread for ?");
 client = Console.ReadLine();
-Console.WriteLine("Noted : The bread is for " + client + "!");
+Console.WriteLine("Noted : The bread is for " + client + "!"); 
+*/
+
+// Exercice 3 - The Thing Namer 3000
+
+Console.WriteLine("What kind of thing are we talking about?");
+string Subject = Console.ReadLine(); // Subject name 
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string Adjective = Console.ReadLine(); // Adjective of subject
+string c = "of Doom"; // String
+string d = "3000"; // String
+Console.WriteLine("The " + Adjective + " " + Subject + " " + c + " " + d + "!");
